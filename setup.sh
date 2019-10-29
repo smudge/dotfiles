e000d394f4ddeaca340416715f52473a032344db
smudge <nathan@ngriffith.com>
2019-10-29 15:03:33 -0400

    Use TMUX Plugin Manager to extend tmux
    Able to clean up some settings with tmux-sensible

--- setup.sh
+++ setup.sh
@@ -10,6 +10,13 @@ nodenv global $LATEST_NODE
 # TODO: Install dotfiles, warn about overwriting changes
 # TODO: Source installed dotfiles
 
+# Install tmux plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# Install tmux plugins (needs a tmux session to attach to)
+tmux start-server
+tmux new-session -d
+~/.tmux/plugins/tpm/scripts/install_plugins.sh
+
 # Support for the Language Server Protocol
 rustup component add rustfmt rls rust-analysis rust-src
 gem install solargraph
