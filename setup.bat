04c639a2c5eb29d31d6b2acafc47bd40a8ae37be
smudge <nathan@ngriffith.com>
2019-11-27 02:41:56 -0500

    Add duplicacy and watchexec to windows (for saved games sync)

--- setup.bat
+++ setup.bat
@@ -1,6 +1,6 @@
 powershell -command "iwr -useb get.scoop.sh | iex"
 call scoop update
-call scoop install coreutils git which sudo openssh neovim
+call scoop install coreutils git which sudo openssh neovim duplicacy watchexec
 call scoop bucket add extras
 call scoop install googlechrome firefox alacritty deluge discord slack sumatrapdf portable-virtualbox vlc plex-player windirstat steam
 
