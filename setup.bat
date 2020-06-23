3d0d7091600051456fd614ad998580c007ffbef5
smudge <nathan@ngriffith.com>
2020-06-23 01:23:52 -0400

    Install ripgrep

--- setup.bat
+++ setup.bat
@@ -1,6 +1,6 @@
 powershell -command "iwr -useb get.scoop.sh | iex"
 call scoop update
-call scoop install coreutils git which sudo openssh neovim duplicacy watchexec fzf nodejs
+call scoop install coreutils git which sudo openssh neovim duplicacy watchexec fzf nodejs ripgrep
 call scoop bucket add extras
 call scoop install googlechrome firefox alacritty deluge discord slack sumatrapdf portable-virtualbox vlc plex-player windirstat steam
 call scoop bucket add nerd-fonts
