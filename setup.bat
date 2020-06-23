bda1efbe46edd554de461f1e01627c3335bae569
smudge <nathan@ngriffith.com>
2020-06-23 01:19:07 -0400

    Install hasklig font

--- setup.bat
+++ setup.bat
@@ -3,5 +3,7 @@ call scoop update
 call scoop install coreutils git which sudo openssh neovim duplicacy watchexec fzf
 call scoop bucket add extras
 call scoop install googlechrome firefox alacritty deluge discord slack sumatrapdf portable-virtualbox vlc plex-player windirstat steam
+call scoop bucket add nerd-fonts
+call sudo scoop install hasklig
 
 REM TODO: install dropbox, keybase, doxie, codec packs like k-lite, and other game platforms
